3d331d245565eb5e47659dee9c5cb894223cbe95
Dmitry Cherkas <cherkas.da@gmail.com>
2015-11-14 14:18:38 -0500

    use ToggleAction for connect/disconnect

--- src/com/intellij/plugins/serialmonitor/ui/actions/ConnectDisconnectAction.java
+++ src/com/intellij/plugins/serialmonitor/ui/actions/ConnectDisconnectAction.java
@@ -2,18 +2,20 @@ package com.intellij.plugins.serialmonitor.ui.actions;
 
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.Presentation;
+import com.intellij.openapi.actionSystem.ToggleAction;
 import com.intellij.openapi.components.ServiceManager;
-import com.intellij.openapi.project.DumbAwareAction;
+import com.intellij.openapi.project.DumbAware;
 import com.intellij.plugins.serialmonitor.service.SerialMonitorSettings;
 import com.intellij.plugins.serialmonitor.service.SerialService;
 import icons.SerialMonitorIcons;
+import org.jetbrains.annotations.NotNull;
 
 import static com.intellij.plugins.serialmonitor.ui.SerialMonitorBundle.message;
 
 /**
  * @author Dmitry_Cherkas
  */
-public class ConnectDisconnectAction extends DumbAwareAction {
+public class ConnectDisconnectAction extends ToggleAction implements DumbAware {
 
     private final SerialService mySerialService = ServiceManager.getService(SerialService.class);
 
@@ -22,20 +24,29 @@ public class ConnectDisconnectAction extends DumbAwareAction {
     }
 
     @Override
-    public void actionPerformed(AnActionEvent e) {
+    public boolean isSelected(AnActionEvent e) {
+        return mySerialService.isConnected();
+    }
+
+    @Override
+    public void setSelected(AnActionEvent e, boolean doConnect) {
         SerialMonitorSettings mySettings = ServiceManager.getService(e.getProject(), SerialMonitorSettings.class);
 
-        if (mySerialService.isConnected()) {
+        if (doConnect) {
+            if (mySettings.isValid()) {
+                // try connect only when settings are known to be valid
+                mySerialService.connect(mySettings.getPortName(), mySettings.getBaudRate());
+            }
+        } else {
             // perform disconnect
             mySerialService.close();
-        } else if (mySettings.isValid()) {
-            // try connect only when settings are known to be valid
-            mySerialService.connect(mySettings.getPortName(), mySettings.getBaudRate());
         }
     }
 
     @Override
-    public void update(AnActionEvent e) {
+    public void update(@NotNull AnActionEvent e) {
+        super.update(e);
+
         SerialMonitorSettings settings = ServiceManager.getService(e.getProject(), SerialMonitorSettings.class);
         Presentation presentation = e.getPresentation();
 
